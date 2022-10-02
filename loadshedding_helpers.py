b5c68ccfe4c4fe0811616b3160284e50591e16d2
Keegan Crankshaw <kcranky@gmail.com>
2022-10-02 11:45:48 +0200

    Rename method to be consistent with naming.

--- loadshedding_helpers.py
+++ loadshedding_helpers.py
@@ -19,20 +19,20 @@ AUTH_HEADER = {'Token': KEYS.sepush_token}
 API_BASE = "https://developer.sepush.co.za/business/2.0/"
 CACHE_NAME = "requests_cache"
 
-def perform_request(endpoint, cache_time=timedelta(days=1)):
+def _perform_request(endpoint, cache_time=timedelta(days=1)):
     session = requests_cache.CachedSession(CACHE_NAME, expire_after=cache_time)
     response = session.get(API_BASE + endpoint, headers={'Token': KEYS.sepush_token})
     return response.json()
 
 
 def find_area(area):
-    res = perform_request("areas_search?text={}".format(area), timedelta(days=10))
+    res = _perform_request("areas_search?text={}".format(area), timedelta(days=10))
     return res
 
 
 def get_status():
-    return perform_request("status", timedelta(hours=1))["status"]
+    return _perform_request("status", timedelta(hours=1))["status"]
 
 
 def get_area_schedule(area_id):
-    return perform_request("area?id={}".format(area_id), cache_time=timedelta(days=6))
+    return _perform_request("area?id={}".format(area_id), cache_time=timedelta(days=6))
